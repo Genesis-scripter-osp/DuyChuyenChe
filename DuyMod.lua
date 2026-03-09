3642b3523dec3249c7c4467675a463119de8f202
Genesis-scripter-osp <zatacutcanh2011@gmail.com>
2026-03-09 07:30:33 +0700

    Update DuyMod.lua

--- DuyMod.lua
+++ DuyMod.lua
@@ -1,50 +1,78 @@
 -- DuyMod Hub
 
 local Hub = {}
-
 Hub.Name = "DuyMod Hub"
 Hub.Version = "1.0"
 
+-- Github Raw Base
 local BASE_URL = "https://raw.githubusercontent.com/Genesis-scripter-osp/DuyChuyenChe/main/"
 
+-- Function load script
 local function Load(path)
     local success, result = pcall(function()
         return loadstring(game:HttpGet(BASE_URL .. path))()
     end)
 
     if success then
+        print("[DuyMod] Loaded:", path)
         return result
     else
-        warn("Failed:", path)
+        warn("[DuyMod] Failed:", path)
         warn(result)
     end
 end
 
 print("Loading "..Hub.Name)
 
--- Config
+-- Wait game load
+repeat task.wait() until game:IsLoaded()
+
+------------------------------------------------
+-- CONFIG
+------------------------------------------------
+
 Load("Config/settings.lua")
 Load("Config/keysystem.lua")
 
--- Core
-Load("Core/Network System")
-Load("Core/Game Loader System")
-Load("Core/Dragon Update System")
+------------------------------------------------
+-- CORE SYSTEM
+------------------------------------------------
 
+Load("Core/Network System.lua")
+Load("Core/Game Loader System.lua")
+Load("Core/Dragon Update System.lua")
+Load("Core/UI Network Visual System.lua")
+
+------------------------------------------------
 -- UI
+------------------------------------------------
+
 Load("UI/Loader.lua")
+
+task.wait(2)
+
 Load("UI/MainUI.lua")
 
--- Modules
-Load("Modules/Combat")
-Load("Modules/ESP")
-Load("Modules/Farming")
-Load("Modules/Misc")
-Load("Modules/Movement")
+------------------------------------------------
+-- MODULES
+------------------------------------------------
 
--- Game Scripts
+Load("Modules/Combat.lua")
+Load("Modules/ESP.lua")
+Load("Modules/Farming.lua")
+Load("Modules/Misc.lua")
+Load("Modules/Movement.lua")
+
+------------------------------------------------
+-- GAME SCRIPTS
+------------------------------------------------
+
 Load("Games/universal.lua")
 
-print("DuyMod Hub Loaded")
+------------------------------------------------
+-- FINISH
+------------------------------------------------
+
+print("DuyMod Hub Loaded Successfully")
 
 return Hub
